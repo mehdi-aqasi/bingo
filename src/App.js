e5207eeff8c9a96fe3605c1818056c1b865d03c5
Mehdi Aghasi <aghasi_m@behsazan.net>
2021-02-11 10:34:39 +0330

    #bingo - sample class

--- src/App.js
+++ src/App.js
@@ -3,6 +3,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="BingoParent">
+        <span className="BingoCellBaseClass">1</span>
+        <span className="BingoCellBaseClass">2</span>
+        <span className="BingoCellBaseClass">3</span>
+        <span className="BingoCellBaseClass">4</span>
+        <span className="BingoCellBaseClass">5</span>
+        <span className="BingoCellBaseClass">6</span>
+        <span className="BingoCellBaseClass">7</span>
+        <span className="BingoCellBaseClass">8</span>
+        <span className="BingoCellBaseClass">9</span>
+        <span className="BingoCellBaseClass">10</span>
+        <span className="BingoCellBaseClass">11</span>
+        <span className="BingoCellBaseClass">12</span>
+        <span className="BingoCellBaseClass">13</span>
+        <span className="BingoCellBaseClass">14</span>
+        <span className="BingoCellBaseClass">15</span>
+        <span className="BingoCellBaseClass">16</span>
+        <span className="BingoCellBaseClass">17</span>
+        <span className="BingoCellBaseClass">18</span>
+        <span className="BingoCellBaseClass">19</span>
+        <span className="BingoCellBaseClass">20</span>
+        <span className="BingoCellBaseClass">21</span>
+        <span className="BingoCellBaseClass">22</span>
+        <span className="BingoCellBaseClass">23</span>
+        <span className="BingoCellBaseClass">24</span>
+        <span className="BingoCellBaseClass">25</span>
+      </div>
     </div>
   );
 }
